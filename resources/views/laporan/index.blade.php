dd4c6a92bb1667e7bcf93e1071fb8e232e789492
yudhavandaru <yudhavandaru20@gmail.com>
2023-03-29 09:30:42 +0700

    ujikompos

---
+++ resources/views/laporan/index.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts.master')
+
+@section('title')
+    Laporan Pendapatan {{ tanggal_indonesia($tanggalAwal, false) }} s/d {{ tanggal_indonesia($tanggalAkhir, false) }}
+@endsection
+
+@push('css')
+<link rel="stylesheet" href="{{ asset('/AdminLTE-2/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}">
+<link rel="stylesheet" href="{{asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{asset('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{asset('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+@endpush
+
+@section('breadcrumb')
+    @parent
+    <li class="active">Laporan </li>
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12">
+        <div class="box">
+            <div class="box-header with-border">
+                <button onclick="updatePeriode()" class="btn btn-info btn-xs btn-flat"><i class="fa fa-plus-circle"></i> Ubah Periode</button>
+                <a href="{{ route('laporan.export_pdf', [$tanggalAwal, $tanggalAkhir]) }}" target="_blank" class="btn btn-success btn-xs btn-flat"><i class="fa fa-file-excel-o"></i> Export PDF</a>
+            </div>
+            <div class="box-body table-responsive">
+                <table class="table table-stiped table-bordered">
+                    <thead>
+                        <th width="5%">No</th>
+                        <th>Tanggal</th>
+                        <th>Penjualan</th>
+                        <th>Pembelian</th>
+                        <th>Pengeluaran</th>
+                        <th>Pendapatan</th>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+@includeIf('laporan.form')
+@endsection
+
+@push('scripts')
+<script src="{{ asset('/AdminLTE-2/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
+
+<script>
+    let table;
+
+//     $(function () {
+//     $(".table").DataTable({
+//       "responsive": true, "lengthChange": false, "autoWidth": false,
+//       "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+//     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+//     $('#example2').DataTable({
+//       "paging": true,
+//       "lengthChange": false,
+//       "searching": false,
+//       "ordering": true,
+//       "info": true,
+//       "autoWidth": false,
+//       "responsive": true,
+//     });
+//   });
+
+    $(function () {
+        table = $('.table').DataTable({
+            responsive: true,
+            processing: true,
+            serverSide: true,
+            autoWidth: false,
+            
+            ajax: {
+                url: '{{ route('laporan.data', [$tanggalAwal, $tanggalAkhir]) }}',
+            },
+            columns: [
+                {data: 'DT_RowIndex', searchable: false, sortable: false},
+                {data: 'tanggal'},
+                {data: 'penjualan'},
+                {data: 'pembelian'},
+                {data: 'pengeluaran'},
+                {data: 'pendapatan'}
+            ],
+            dom: 'Brt',
+            bSort: false,
+            bPaginate: false,
+        });
+
+        $('.datepicker').datepicker({
+            format: 'yyyy-mm-dd',
+            autoclose: true
+        });
+    });
+
+    function updatePeriode() {
+        $('#modal-form').modal('show');
+    }
+</script>
+
+@endpush
